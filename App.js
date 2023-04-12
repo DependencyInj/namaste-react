c6a3818fdc6809fc27b598ea35c2d3f93ab60a7e
DependencyInj <somdevhari.learning@gmail.com>
2023-04-12 11:52:52 +0530

    playig with functional component

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ import ReactDOM from 'react-dom';
 
 const jsxHeading = <h1 id="headin">React and Akshay saini is amazing</h1>;
 
+const FunctionalHeading = () => {
+    return (
+        <div>
+            <h1>
+                Namaste React fucntional component.🚀
+            </h1>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<FunctionalHeading/>);
