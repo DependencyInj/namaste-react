a989d746ecb159611c3468da8ae4b3c33fb938ff
DependencyInj <somdevhari.learning@gmail.com>
2023-04-13 11:59:33 +0530

    trying out react fragments

--- App.js
+++ App.js
@@ -6,14 +6,21 @@ const CompositeHeading = () => <h1>This is going to be a composite functional co
 
 const FunctionalHeading = () => {
     return (
-        <div>
-            <CompositeHeading/>
-            <h1>
-                Namaste React fucntional component.🚀
-            </h1>
-        </div>
+        <>
+            <div>
+                <p>This is a parent</p>
+                <CompositeHeading />
+                <h1>
+                    Namaste React fucntional component.🚀
+                </h1>
+            </div>
+            <>
+            <h1>Fragment inside a fragment</h1>
+            </>
+            <div>This is another parent</div>
+        </>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FunctionalHeading/>);
+root.render(<FunctionalHeading />);
