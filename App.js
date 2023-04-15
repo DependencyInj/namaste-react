dadb70a4155b27add572346a56298593527ac604
DependencyInj <somdevhari.learning@gmail.com>
2023-04-15 07:49:21 +0530

    adding awesome card styling

--- App.js
+++ App.js
@@ -12,10 +12,31 @@ const Header = () => {
             <div className='nav-items'>
                 <ul>
                     <li>Home</li>
-                    <li>About</li>  
-                    <li>Contact Us</li>  
+                    <li>About</li>
+                    <li>Contact Us</li>
                     <li>Cart</li>
                 </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurentCard = () => {
+    return (
+        <div className='card'>
+            <img className='res-logo' src='https://b.zmtcdn.com/data/pictures/9/20204179/f940438a7dd0aeb302a5e47925ec1eea_o2_featured_v2.jpg?output-format=webp' />
+            <h3>Arabian Restaurant</h3>
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                <RestaurentCard />
             </div>
         </div>
     )
@@ -25,7 +46,8 @@ const Header = () => {
 const AppLayout = () => {
     return (
         <div className='app'>
-            <Header/>
+            <Header />
+            <Body />
         </div>
     )
 }
