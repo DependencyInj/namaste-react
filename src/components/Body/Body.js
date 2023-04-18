b2ff2311d6b880c8b390cc53f28062058f5b896a
DependencyInj <somdevhari.learning@gmail.com>
2023-04-18 23:14:49 +0530

    adding search

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,16 +6,30 @@ import { useState } from 'react';
 
 const Body = () => {
     const [listOfres, setListOfRes] = useState(resList);
+    const [searchText, setSearchText] = useState("");
+
+    const search = () => {
+        setListOfRes(listOfres
+            .filter(item => item.data.name
+                .toLowerCase()
+                .indexOf(searchText.toLowerCase()) > -1));
+    }
 
     const filterTopRated = () => {
         setListOfRes(listOfres.filter(item => item.data.avgRating > 4));
     }
     return (
         <div className='body'>
-            <button onClick={filterTopRated} className='top-rate-btn'>
-                <img className='top-rated-icon' src={topRatedIcon} />
-                Top rated
-            </button>
+            <div>
+                <button onClick={filterTopRated} className='top-rate-btn'>
+                    <img className='top-rated-icon' src={topRatedIcon} />
+                    Top rated
+                </button>
+                <div className='search-container'>
+                    <input onChange={(event) => setSearchText(event.target.value)} value={searchText} className='search-text' />
+                    <button onClick={() => search()} className='search-button'>Search</button>
+                </div>
+            </div>
             <div className='res-container'>
                 {
                     listOfres.map(res => <RestaurentCard key={res.data.id} resData={res} />)
