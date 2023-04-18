e39618883bf813d33516207bd350f17438cfe865
DependencyInj <somdevhari.learning@gmail.com>
2023-04-18 07:59:54 +0530

    playing with hooks, useState

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,11 @@
+import { CDN_URL } from '../../utils/constants';
 import './Card.css'
 
 const RestaurentCard = (props) => {
     const { name, cuisines, avgRating, cloudinaryImageId } = props.resData?.data;
     return (
         <div className='card'>
-            <img className='res-logo' src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`} />
+            <img className='res-logo' src={`${CDN_URL}${cloudinaryImageId}`} />
             <h3>{name}</h3>
             <h5>{cuisines.join(', ')}</h5>
             <h5>{avgRating} Stars</h5>
