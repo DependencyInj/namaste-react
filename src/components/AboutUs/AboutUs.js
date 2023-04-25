4279cd54d646aecf9116c281f7293820c6a0cb98
DependencyInj <somdevhari.learning@gmail.com>
2023-04-25 11:38:22 +0530

    exploring the class based components

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,8 +1,11 @@
+import Profile from "../Profile/Profile";
+
 const AboutUs = () => {
     return (
         <div>
             <h1>About Us</h1>
             <div>This is a food orderng app sample about page.</div>
+            <Profile />
         </div>
     )
 }
