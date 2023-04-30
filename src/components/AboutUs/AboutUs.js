228b55d6a374b184fe3c409d2313f9151e539b66
DependencyInj <somdevhari.learning@gmail.com>
2023-04-30 09:52:55 +0530

    calling github profile api in component did mount of child and playing around

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,13 +1,25 @@
 import Profile from "../Profile/Profile";
+import React from "react";
 
-const AboutUs = () => {
-    return (
-        <div>
-            <h1>About Us</h1>
-            <div>This is a food orderng app sample about page.</div>
-            <Profile name="Harikrishnan Soman" />
-        </div>
-    )
+class AboutUs extends React.Component {
+    constructor(props){
+        super(props);
+        console.log("Parent constructor");
+    }
+
+    componentDidMount(){
+        console.log("Parent componentDidMount");
+    }
+    render(){
+        console.log("Parent render");
+        return (
+            <div>
+                <h1>About Us</h1>
+                <div>This is a food orderng app sample about page.</div>
+                <Profile name="Harikrishnan Soman" />
+            </div>
+        )
+    }
 }
 
 export default AboutUs;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,28 +6,42 @@ class Profile extends React.Component {
         super(props);
 
         this.state = {
-            count: 0
+            count: 0,
+            name:"",
+            userName:"",
+            imgUrl:"",
         }
 
-        console.log("Constructing Profile");
+        console.log("child Constructing Profile");
     }
 
-    componentDidMount() {
-        console.log("Mounting Profile...");
+    async componentDidMount() {
+        const data = await fetch('https://api.github.com/users/DependencyInj');
+        const json = await data.json();
+        this.setState({
+            name: json.name,
+            userName: json.login,
+            imgUrl: json.avatar_url
+        })
+        console.log("json", json);
+        console.log("child Mounting Profile...");
     }
-    
+
     render() {
-        console.log("Rendering Profile...");
+        console.log("child Rendering Profile...");
         return (
             <div>
-                <h1>Profile of {this.props.name}</h1>
-                <h2>Count: {this.state.count}</h2>
+                <h1>Profile of {this.state.name}</h1>
+                <h2>UserName: {this.state.userName}</h2>
+                <img src={this.state.imgUrl}/>
                 <button onClick={() => this.setState({
                     count: this.state.count + 1
                 })}>Set count</button>
             </div>
         );
     }
+
+    
 }
 
 export default Profile;
