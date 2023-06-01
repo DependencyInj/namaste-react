0f270426e5ee97dec5fb2e7784e02cfdebe5ccf0
DependencyInj <somdevhari.learning@gmail.com>
2023-06-01 11:24:54 +0530

    adding clear cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,23 @@
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import CartItem from '../CartItem/CartItem';
+import { clearCart } from '../../slices/CartSlice';
 
 const Cart = () => {
 
     const cartItems = useSelector(store => store.cart.items);
-    console.log(cartItems);
+    const dispatch = useDispatch();
+    
+    const handleClear = () => {
+        dispatch(clearCart())
+    }
+    
     return (
         <div>
             <h1 className="font-bold text-3xl">My cart - {cartItems.length}</h1>
+            <div>
+                <button className='bg-green-300' onClick={() => handleClear()}>Clear cart</button>
+            </div>
             <div className='flex'>
                 {cartItems?.map((item, index) =>
                     <CartItem key={item.id} item={item} />
--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -3,7 +3,7 @@ const CartItem = (props) => {
     const { name, price, imageId, id } = props.item;
     console.log("cart item", props.item)
     return (
-        <div className="m-3 p-2 h-80 rounded-md bg-slate-50 hover:shadow-md w-48">
+        <div className="m-3 p-2 h-80 rounded-md bg-slate-100 hover:drop-shadow-xl w-48">
             <img className='h-32 w-48' src={`${CDN_URL}${imageId}`} />
             <p>{name}</p>
             <p>Price: {price/100}</p>
